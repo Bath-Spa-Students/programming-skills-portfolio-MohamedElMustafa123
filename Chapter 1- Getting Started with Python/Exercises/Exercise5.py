b9a39d895c26dfe69c39b2d2de89cedde33d50fd
MohamedElMustafa123 <nobimohamed262@gmail.com>
2023-09-30 15:33:11 +0400

    Exercise 5.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise5.py
@@ -0,0 +1,5 @@
+print("Hello, this program calculates the area of a circle!")
+radius = float(input("Enter the radius of the circle: "))
+#takes user input and stores it in a variable 
+area = 3.141 * radius**2
+print("The area of the circle is: " + str(area))
