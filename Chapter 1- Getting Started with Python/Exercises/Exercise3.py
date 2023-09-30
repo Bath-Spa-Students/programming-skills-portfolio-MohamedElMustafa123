e1c030afc938855ee542bf17ddb37d121e8185d2
MohamedElMustafa123 <nobimohamed262@gmail.com>
2023-09-30 14:59:02 +0400

    Exercise 3

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise3.py
@@ -0,0 +1,4 @@
+import datetime
+date_and_time = datetime.datetime.now()
+#.now function retuns the date and time with the current local date and time
+print("Today's date and time is " + str(date_and_time))
