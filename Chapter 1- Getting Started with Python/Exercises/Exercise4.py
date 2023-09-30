3a6f98c89c5504d3129ebfb1bfb3f703cca6fc27
MohamedElMustafa123 <nobimohamed262@gmail.com>
2023-09-30 15:07:41 +0400

    Exercise 4.py

---
+++ Chapter 1- Getting Started with Python/Exercises/Exercise4.py
@@ -0,0 +1,5 @@
+#Write three strings in different variables and print the output as one string.
+intro = "Hi my name is "
+name = "Mohamed ElMustafa, "
+hobby = "I enjoy skating"
+print(intro + name + hobby)
